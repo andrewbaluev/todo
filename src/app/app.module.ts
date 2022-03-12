18fa2b7330bfa3e8379dedd216a1fb69ae1e8f04
Andrew Baluev <damico750@yandex.ru>
2022-03-12 18:10:07 +0300

    добавил компонент categiries, севис data-handler, выполнил декомпозицию шаблона html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CategoriesComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
